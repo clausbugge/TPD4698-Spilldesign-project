9a476d10dbf7fe3b1fe377baefe93f82c8891485
Henning Luick <henningel@gmail.com>
2017-11-11 20:23:46 +0100

    Particle dash. Particle indicator for ghost. added "point of interest(exclamation point)". dynamic camera distance, fixed toggle on trigger script ( no idea why it worked before)

---
+++ Assets/sinContinuatedBounce.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sinContinuatedBounce : MonoBehaviour {
+
+    public float loopTime;
+    public float bounceDist;
+    private Vector3 startPos;
+    // Use this for initialization
+    void Start () {
+        startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newPos = startPos;
+        newPos.y += Mathf.Sin((Time.time * 90 * 1.0f / loopTime) * Mathf.Deg2Rad) * bounceDist;
+        transform.position = newPos;
+	}
+}
---
+++ Assets/triggerOnCollision.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerOnCollision : MonoBehaviour {
+
+    public List<GameObject> triggerList;
+
+    // Use this for initialization
+    void Start () {
+        //triggerList = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            foreach (GameObject obj in triggerList)
+            {
+                if (obj != null)
+                {
+                    obj.GetComponent<Triggerable>().startTrigger();
+                }
+            }
+        }
+    }
+
+}
